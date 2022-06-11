caba54ac8f7498496f03c24c40ee004ea39e7723
Nattapon Hoedkhunthod <70090705+zx4545zx@users.noreply.github.com>
2022-06-11 21:57:14 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
